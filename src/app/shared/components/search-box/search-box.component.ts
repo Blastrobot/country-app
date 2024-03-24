7073e8eba6f142b1cacb75b51b9e56d32d719e2a
Blastrobot <carlosjcv94@gmail.com>
2024-03-24 01:18:22 +0100

    Add debouncer to emit values for search after a small delay

--- src/app/shared/components/search-box/search-box.component.ts
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -1,4 +1,5 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { Subject, Subscription, debounceTime } from 'rxjs';
 
 @Component({
   selector: 'shared-search-box',
@@ -6,14 +7,37 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   styles: [
   ]
 })
-export class SearchBoxComponent {
-
+export class SearchBoxComponent implements OnInit, OnDestroy {
+  
+  private debouncer: Subject<string> = new Subject<string>();
+  private debouncerSubscription?: Subscription;
+  
   @Input() public placeholder: string = '';
-
+  
   @Output() public onSearchValue = new EventEmitter<string>();
+
+  @Output() public onDebounce = new EventEmitter<string>();
+  
+  ngOnInit(): void {
+    this.debouncer
+      .pipe(
+        debounceTime(500)
+      )
+      .subscribe( value => {
+        this.onDebounce.emit( value );
+      });
+  };
+
+  ngOnDestroy(): void {
+    this.debouncerSubscription?.unsubscribe();
+  };
 
   public emitSearchValue( value: string ) {
     this.onSearchValue.emit( value );
   }
 
+  public onKeyPress (searchTerm: string) {
+    // console.log(searchTerm);
+    this.debouncer.next(searchTerm);
+  }
 }
